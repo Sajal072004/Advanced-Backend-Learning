7faa2340d46db1135ffb0834fc524db51a7f2162
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-12-10 17:39:15 +0530

    implemented sockets

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const http = require('http');
+const {Server} =require('socket.io')
 
 const app = express();
 const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on('connection' , (client) => {
+    
+    client.on('user-message', (data) => {
+        console.log("new message " ,data);
+        io.emit("message",data);
+        
+    })
+
+})
+
+
 
 app.use(express.static("/public"));
 
